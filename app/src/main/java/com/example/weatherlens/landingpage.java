793be94c816f0841b5ce1b404a10f9d1a94417c0
IT20648190 <it20648190@my.sliit.lk>
2022-11-10 16:25:21 +0530

    UI

---
+++ app/src/main/java/com/example/weatherlens/landingpage.java
@@ -0,0 +1,14 @@
+package com.example.weatherlens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class landingpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landingpage);
+    }
+}
